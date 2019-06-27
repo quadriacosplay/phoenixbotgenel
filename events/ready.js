b16bddfa00c5e53b0e4efff2bd58dae97e293ebb
quadriacosplay <52280615+quadriacosplay@users.noreply.github.com>
2019-06-27 14:10:14 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-  client.user.setPresence({game:{name:`Phoenix Roleplay | Yardım komutları İçin ${prefix}yardım | ${client.guilds.size} Sunucuda hizmet vermekte | ${client.users.size} Kullanıcı | Yeni Komutlar ${prefix}yardım | Versiyon 3.1 | 2019`, type: 0}});
+  client.user.setPresence({game:{name:`Phoenix Roleplay | Yardım komutları İçin ${prefix}yardım | ${client.guilds.size} Sunucuda hizmet vermekte | ${client.users.size} Kullanıcı | Yeni Komutlar ${prefix}yardım | Versiyon 3.1`, type: 0}});
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
